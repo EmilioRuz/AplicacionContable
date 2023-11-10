bf2085c6cf157609b26cc8c02142993f536f1895
Emilio Ruz <21470096@Campeche.tecnm.mx>
2023-11-09 21:06:34 -0600

    Aplicacion contable en xaml

---
+++ ContadorManual.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace ContadorManual;
+
+public partial class MainPage : ContentPage
+{
+    private int conteo;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        conteo = 0;
+        ConteoLabel.Text = conteo.ToString();
+    }
+
+    private void ContarButton(object sender, EventArgs e)
+    {
+        conteo++;
+        ConteoLabel.Text = conteo.ToString();
+    }
+
+    private void ReiniciarButton(object sender, EventArgs e)
+    {
+        conteo = 0;
+        ConteoLabel.Text = conteo.ToString();
+    }
+}
